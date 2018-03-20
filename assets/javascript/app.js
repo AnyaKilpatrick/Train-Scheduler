544dbd053cf6c4ba0785077103d44f8d304bcd09
Anya <hannakilpatrick@gmail.com>
2018-03-20 15:38:47 -0400

    calculate minutes and update text on screen every minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,15 @@
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBx-CqbRhUCw3eVByMPypojbFgMurS-8tc",
-    authDomain: "myfirsttrainscheduler.firebaseapp.com",
-    databaseURL: "https://myfirsttrainscheduler.firebaseio.com",
-    projectId: "myfirsttrainscheduler",
-    storageBucket: "",
-    messagingSenderId: "1044035882495"
-  };
-  firebase.initializeApp(config);
-
+  apiKey: "AIzaSyBx-CqbRhUCw3eVByMPypojbFgMurS-8tc",
+  authDomain: "myfirsttrainscheduler.firebaseapp.com",
+  databaseURL: "https://myfirsttrainscheduler.firebaseio.com",
+  projectId: "myfirsttrainscheduler",
+  storageBucket: "",
+  messagingSenderId: "1044035882495"
+};
+firebase.initializeApp(config);
 var database = firebase.database();
-
 // Initial Variables
 var trainname;
 var destination;
@@ -37,60 +35,68 @@ var scheduler = {
         destination: destination,
         frequency: frequency,
         arrival: arrival,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
       })
     })
   },
-
   addInfo: function(){
     database.ref().on("child_added", function(childSnapshot) {
+      var childTime;
+      var parseTime;
+      var childFrequency;
+      var parseFrequency;
+      var now;
+      var firstTimeConverted;
+      var diffTime;;
+      var timeRemainder;
+      var minutesAway;
+
+      function countTime(){
+      childTime = childSnapshot.val().arrival; //first arrival time
+      parseTime = moment(childTime, "HH:mm"); // time in miliseconds
+      childFrequency = childSnapshot.val().frequency;
+      parseFrequency = moment(childFrequency, "m");
     
-      var childTime = childSnapshot.val().arrival; //military time
-      var parseTime = moment(childTime, "LT"); // time in miliseconds
-    
-      var childFrequency = childSnapshot.val().frequency;
-      var parseFrequency = moment(childFrequency, "m");
-    
-      var now = moment();
-      var minutesAway = parseTime.diff(now, "minutes");//calculates the difference between "now" time and "next arrival" time
-    
-      if (minutesAway === 0){ //if "now"time is the same as "arrival" time, then..
-        console.log("you missed your train");
-        console.log("arrival time: "+childTime);
-        console.log("first arrival: "+ childSnapshot.val().arrival);
-        childTime = moment(childTime, 'LT').add(childFrequency, 'minutes').format("LT"); //add frequency (min) to "arrival" time
+      now = moment();
+      firstTimeConverted = moment(childTime, "HH:mm").subtract(1, "years"); // First Time (pushed back 1 year to make sure it comes before current time)
+      diffTime = moment().diff(moment(firstTimeConverted), "minutes"); // Difference between the times
         
-        
-        parseTime = moment(childTime, "LT") //update var values
-        minutesAway = parseTime.diff(now, "minutes");
-        console.log("new arrival time: !!!" + childTime);
-        console.log("first arrival time: "+ childSnapshot.val().arrival);
-    }
+      timeRemainder = diffTime % childFrequency;// Time apart (remainder)
+      minutesAway = childFrequency - timeRemainder; // Minutes Until Next Train
+
+      childTime = moment().add(minutesAway, "minutes").format("LT"); // Next Arrival Time
+      console.log("ARRIVAL TIME: " + moment(childTime, "LT").format("LT"));
+      }
+      countTime();
+      
       $("#trainInfo").append(`
           <tr>
           <td>${childSnapshot.val().name}</td>
           <td>${childSnapshot.val().destination}</td>
           <td>${childSnapshot.val().frequency}</td>
-          <td>${childTime}</td>
-          <td>${minutesAway}</td>
+          <td id="${childSnapshot.val().dateAdded}arrival">${childTime}</td>
+          <td id="${childSnapshot.val().dateAdded}">${minutesAway}</td>
           </tr>
       
       `)
+      function updateTime() { //now we are updating minutesAway and childTime variables on screen every minute
+        countTime();
+        var grabId = childSnapshot.val().dateAdded;
+        document.getElementById(grabId).innerHTML=minutesAway;
+        
+        var grabArrivalId = childSnapshot.val().dateAdded;
+        document.getElementById(grabId + "arrival").innerHTML=childTime;
+
+        setTimeout(updateTime, 60000);
+      }
+      updateTime();
+      
       // If any errors are experienced, log them to console.
       }, function(errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
-  }
-
+  },
 }
 
 scheduler.addTrain();
 scheduler.addInfo();
-
-
-
-
-// var t;
-// $(document).ready(function(){
-//   t = setInterval(childSnapshot,60000);
-// })
